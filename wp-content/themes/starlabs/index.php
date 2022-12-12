201f64b02c8d7deefa60ac2f2dbc784374f06c75
sarabajraktari <sarabajraktari90@gmail.com>
2022-12-12 17:05:42 +0100

    the team cards

--- wp-content/themes/starlabs/index.php
+++ wp-content/themes/starlabs/index.php
@@ -1,11 +1,11 @@
 <?php get_header();?>
 <p class="w-12"></p>
 <?php 
-   $field = get_field('modules');
-   if (is_array($field)) {
-        foreach ($field as $key => $value) {
-            $module = $value[$value['acf_fc_layout']];
-            include (get_template_directory().'/modules/'.$value['acf_fc_layout'].'.php');
-        }
-   }
+//    $field = get_field('modules');
+//    if (is_array($field)) {
+//         foreach ($field as $key => $value) {
+//             $module = $value[$value['acf_fc_layout']];
+//             include (get_template_directory().'/modules/'.$value['acf_fc_layout'].'.php');
+//         }
+//    }
 get_footer();

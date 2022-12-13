cf2d9b479c1fdf7e528317dd4c5689600225b284
ardialiqkaj <ardi.aliqkaj@gmail.com>
2022-12-13 12:46:46 +0100

    User-Reactions-Module

--- wp-content/themes/starlabs/header.php
+++ wp-content/themes/starlabs/header.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <title><?php wp_title( '|', true, 'right' ); ?></title>
-        <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
-        <?php wp_head(); ?>
-    </head>
-<body>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_uri()); ?>" type="text/css" />
+    <?php wp_head(); ?>
+</head>
+
+<body class="bg-gray-100">
---
+++ wp-content/themes/starlabs/modules/user_reactions.php
@@ -0,0 +1,44 @@
+<div class="flex flex-wrap justify-center mx-40 max-lg:mx-0">
+    <div class="bg-white my-10 w-96 mx-4 px-10 py-6 rounded-3xl max-lg:w-3/4">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fill-blue-600 w-8 h-8">
+            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+            <path d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" />
+        </svg>
+        <p class="text-gray-400 pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tristique senectus et netus et malesuada fames ac turpis. Molestie ac feugiat sed lectus vestibulum mattis.</p>
+        <div class="flex">
+            <img src="" alt="profile">
+            <div class="pl-6">
+                <p class="font-bold">Ardi Alickaj</p>
+                <p class="text-gray-400">Full-Stack Intern</p>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white my-10 w-96 mx-4 px-10 py-6 rounded-3xl max-lg:w-3/4">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fill-blue-600 w-8 h-8">
+            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+            <path d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" />
+        </svg>
+        <p class="text-gray-400 pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi porta lorem mollis aliquam ut porttitor leo a diam. Sit amet dolor morbi non arcu.</p>
+        <div class="flex">
+            <img src="" alt="profile" width="60px" height="60px">
+            <div class="pl-6">
+                <p class="font-bold">Ardi Alickaj</p>
+                <p class="text-gray-400">Full-Stack Intern</p>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white my-10 w-96 mx-4 px-10 py-6 rounded-3xl max-lg:w-3/4">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fill-blue-600 w-8 h-8">
+            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+            <path d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" />
+        </svg>
+        <p class="text-gray-400 pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi porta lorem mollis aliquam ut porttitor leo a diam. Sit amet dolor morbi non arcu.</p>
+        <div class="flex">
+            <img src="" alt="profile" width="60px" height="60px">
+            <div class="pl-6">
+                <p class="font-bold">Ardi Alickaj</p>
+                <p class="text-gray-400">Full-Stack Intern</p>
+            </div>
+        </div>
+    </div>
+</div>

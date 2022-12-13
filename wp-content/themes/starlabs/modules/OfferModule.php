e00e8bbb32acc39cddfd65b2eb5c26b51b16e9fc
Rinor Gerxhaliu <rinor.gerxhaliu@catenamedia.com>
2022-12-13 16:04:20 +0100

    fixes

--- wp-content/themes/starlabs/modules/OfferModule.php
+++ wp-content/themes/starlabs/modules/OfferModule.php
@@ -1,4 +1,4 @@
-<body class="bg-blue-800">
+<div class="bg-blue-800">
     <h1 class="font-semibold flex justify-center text-base my-8 text-white">Get Started</h1><br>
 
     <h1 class="flex justify-center text-white text-[40px]">Simple, Transparent Pricing</h1>
@@ -11,4 +11,4 @@
         <button class=" w-40 h-10 bg-white rounded-full hover:bg-sky-700"><h3 class=" text-slate-500	">Get started</h3></button>
     </div>
     <p class="text-white flex justify-center">Copyright 2021 nicepage.com • <a class="underline underline-offset-1" href=""> Terms of Service • </a> <a class="underline underline-offset-1" href=""> Privacy Policy</a></p>
-</body>
+</div>

9ce0655cfebfc3601b882b5fe8289a7b974ac19e
AbedinTelaku <at45490@ubt-uni.net>
2022-12-13 14:37:00 +0100

    OfferModule update

--- wp-content/themes/starlabs/modules/module2.php
+++ wp-content/themes/starlabs/modules/module2.php
@@ -1,1 +1,2 @@
-<h1 class=" font-bold text-lg text-red-200"><?= $module['title'];?></h1>
+<h1 class="text-red-800 font-bold text-lg border-y-4"><?= $module['test'];?></h1>
+<p class="px-8 text-green-500 text-xl">test</p>

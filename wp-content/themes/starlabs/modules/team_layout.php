a8ded3304155594667511369d8a908eae5f1def6
sarabajraktari <sarabajraktari90@gmail.com>
2022-12-14 14:06:09 +0100

    changes

--- wp-content/themes/starlabs/modules/team_layout.php
+++ wp-content/themes/starlabs/modules/team_layout.php
@@ -18,17 +18,16 @@ get_header();
         <div class=" flex flex-wrap justify-center ">
 
             <?php foreach($team as $value):?>
-
             <div class="container w-full max-w-md bg-white  border-none rounded-3xl m-5 p-5">
                 <div class="flex flex-col space-y-5 md:space-y-0 md:space-x-6 md:flex-row ">
                     <?php if($value['image']): ?>
                     <img src="<?php echo $value['image']['url']; ?>" alt=""
-                        class="self-center overflow-auto flex-shrink-0 w-32 h-32 border rounded-full md:justify-self-start md:self-top">
+                        class="self-center flex-shrink-0 w-40 h-40 border rounded-full md:justify-self-start">
                     <?php else:?>
                     <img src="<?php  echo $default_pic ?>" alt=""
-                        class="self-center flex-shrink-0 w-32 h-32 border rounded-full md:justify-self-start ">
+                        class="self-center flex-shrink-0 w-40 h-40 border rounded-full md:justify-self-start">
                     <?php endif;?>
-                    <div class="flex flex-col space-y-1 ">
+                    <div class="flex flex-col space-y-2 ">
                         <span
                             class="text-slate-300 font-semibold text-center md:text-left"><?php  echo $value['job_role'];?>
                         </span>
@@ -38,10 +37,10 @@ get_header();
                         <p class="text-slate-400 text-sm"><?php echo $value['description'];?></p>
                     </div>
                 </div>
-                <div class="flex justify-center pt-4 space-x-4 align-center">
+                <div class="flex justify-center pt-4  align-center">
 
-                    <a rel="noopener noreferrer" href="<?php echo $value['link']['url'] ?>"
-                        target=<?php echo $value['link']['target'] ?> aria-label="Dribble"
+                    <a rel="noopener noreferrer" href="<?php echo $value['facebook_url']['url'] ?>"
+                        target=<?php echo $value['facebook_url']['target'] ?> aria-label="Dribble"
                         class="p-1 rounded-full bg-[#4767C9] ">
                         <svg class="h-6 w-6 text-white" width="6" height="6" viewBox="0 0 24 24" stroke-width="1"
                             stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -49,14 +48,18 @@ get_header();
                             <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
                         </svg>
                     </a>
-                    <a rel="noopener noreferrer" href="#" aria-label="Twitter" class="p-1 rounded-full bg-[#4767C9] ">
+                    <a rel="noopener noreferrer" href="<?php echo $value['twitter_url']['url'] ?>"
+                        target=<?php echo $value['twitter_url']['target'] ?> aria-label="Twitter"
+                        class="p-1 rounded-full bg-[#4767C9] ">
                         <svg class="h-6 w-6 text-white " viewBox="0 0 24 24" fill="white" stroke="currentColor"
                             stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                             <path
                                 d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
                         </svg>
                     </a>
-                    <a rel="noopener noreferrer" href="#" aria-label="Email" class="p-1 rounded-full bg-[#4767C9]">
+                    <a rel="noopener noreferrer" href="<?php echo $value['instagram_url']['url'] ?>"
+                        target=<?php echo $value['instagram_url']['target'] ?> aria-label="Email"
+                        class="p-1 rounded-full bg-[#4767C9]">
                         <svg class="h-6 w-6 text-white" width="4" height="4" viewBox="0 0 24 24" stroke-width="1"
                             stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" />

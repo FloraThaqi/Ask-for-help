04f1ef95088bf0125dd3f632f4644300d775381a
denisademaku1 <73554758+denisademaku1@users.noreply.github.com>
2022-12-16 00:31:12 +0100

    Cards Module

---
+++ wp-content/themes/starlabs/modules/CardsModule.php
@@ -0,0 +1,73 @@
+    <div class="mt-20">
+      <h1 class="max-w-md mx-auto text-4xl font-extrabold text-center">
+        The New Product Development Process
+      </h1>
+
+      <div
+        class="sm:px-5 md:px-0 mt-10 sm:mx-20 md:mx-auto flex flex-wrap gap-5 max-w-full justify-center"
+      >
+        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+          <div class="space-y-2">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon1.png' ); ?>" class="w-16 h-16" />
+            <h2 class="font-semibold">Research user needs</h2>
+          </div>
+          <div class="pt-5 text-gray-400">
+            <p>Discover pain points and understand user behaviour</p>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+          <div class="space-y-2">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon2.png' ); ?>" class="w-16 h-16" />
+            <h2 class="font-semibold">Idea Generation</h2>
+          </div>
+          <div class="pt-5 text-gray-400">
+            <p>Become a category leader using designops and UX/UI design</p>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+          <div class="space-y-2">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon3.png' ); ?>" class="w-16 h-16" />
+
+            <h2 class="font-semibold">Prototype an idea</h2>
+          </div>
+          <div class="pt-5 text-gray-400">
+            <p>Validate ideas with an interactive prototype of your vision</p>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+          <div class="space-y-2">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon4.png' ); ?>" class="w-16 h-16" />
+
+            <h2 class="font-semibold">Design a new product</h2>
+          </div>
+          <div class="pt-5 text-gray-400">
+            <p>Launch an MVP with a best in class user experience</p>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+          <div class="space-y-2">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon5.png' ); ?>" class="w-16 h-16" />
+            <h2 class="font-semibold">Sourcing</h2>
+          </div>
+          <div class="pt-5 text-gray-400">
+            <p>
+              Once you have a product prototype you're satisfied with, it's time
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+          <div class="space-y-2">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon6.png' ); ?>" class="w-16 h-16" />
+            <h2 class="font-semibold">Build Marketing Plan</h2>
+          </div>
+          <div class="pt-5 text-gray-400">
+            <p>Easiest way to manage your projects and tasks</p>
+          </div>
+        </div>
+      </div>
+    </div>

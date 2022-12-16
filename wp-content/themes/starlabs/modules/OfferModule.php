d56d234ee88b1e625deed16c824b1d08e8b6eb79
Rinor Gerxhaliu <75761239+rinori-g@users.noreply.github.com>
2022-12-16 16:06:24 +0100

    merging

--- wp-content/themes/starlabs/modules/OfferModule.php
+++ wp-content/themes/starlabs/modules/OfferModule.php
@@ -9,34 +9,18 @@
     $terms = $module['terms'];
     $privacy_policy = $module['privacy_policy'];
     
-    if($title): ?>
-    <h5 class=" mt-16 pt-12 leading-3 space-y-1.5 font-semibold flex justify-center max-lg:w-11/12 text-lg text-white"><?php echo $title ?></h5><br> 
-    <?php endif; ?>
-    
-    <?php
-    if($sub_title): ?>
-    <h2 class=" max-lg:ml-4 flex justify-center text-white  text-[40px] "><?php echo $sub_title ?></h2>
-    <?php endif; ?>
-
-    <?php
-    if($offer): ?>
-    <h1 class=" font-bold  flex justify-center text-white text-[70px]"><?php echo $offer ?></h1>
-    <?php endif; ?>
-
-    <?php
-    if($description): ?>
-    <p class="tracking-tight text-white flex justify-center text-xl	"><?php echo $description ?></p>
-    <?php endif; ?>
-
-    <div class=" my-8 text-white flex justify-center">
-        <?php 
-         if($button): ?>
+?>
+<div class="bg-indigo-500" >
+    <div class="py-14">
+        <h5 class=" leading-3 space-y-1.5 font-semibold flex justify-center text-lg	 text-white"><?php echo $title ?></h5><br>
+        <h2 class=" flex justify-center text-white text-[30px]"><?php echo $sub_title ?></h2>
+        <h1 class=" font-bold  flex justify-center text-white text-[70px]"><?php echo $offer ?></h1>
+        <p class="tracking-tight text-white flex justify-center px-6 text-center"><?php echo $description ?></p>
+        <div class=" my-10 text-white flex justify-center">
+        <?php if($button): ?>
         <a href="<?php echo $button ?>" target="_blank" class="self-center font-bold bg-white rounded-full hover:bg-sky-700"><h3 class="px-20 py-4 text-lg font-bold text-slate-500"><?php echo $button ?></h3></a>
-        <?php endif ?>
-    </div>
-    
-
-    <div class="text-[14px] text-white flex justify-center">
+        <?php endif ?>        </div>
+        <div class="text-[14px] text-white flex justify-center">
     <?php 
         if( $copyright ): ?>
     <p class="text-white flex justify-center"><?php echo $copyright ?>  
@@ -52,5 +36,7 @@
         if( $privacy_policy ): ?> 
      <a class="ml-1 underline underline-offset-1">  <?php echo $privacy_policy ?></a></p>
      <?php endif; ?>
+    </div>
+         </div>
     </div>
 </div>

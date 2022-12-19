dbb1d8a901cffd438fb97e30c2c3b90018442ab2
Rinor Gerxhaliu <75761239+rinori-g@users.noreply.github.com>
2022-12-19 09:37:57 +0100

    merging

--- wp-content/themes/starlabs/modules/CardsModule.php
+++ wp-content/themes/starlabs/modules/CardsModule.php
@@ -1,12 +1,12 @@
-    <div class="mt-20">
-      <h1 class="max-w-md mx-auto text-4xl font-extrabold text-center">
+<div class="mt-20">
+      <h1 class="mt-8 mx-auto max-w-md text-4xl font-bold text-center">
         The New Product Development Process
       </h1>
 
       <div
-        class="sm:px-5 md:px-0 mt-10 sm:mx-20 md:mx-auto flex flex-wrap gap-5 max-w-full justify-center"
+        class="container mx-auto grid sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 mb-14"
       >
-        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+        <div class="bg-white rounded-md p-5 shadow-lg">
           <div class="space-y-2">
             <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon1.png' ); ?>" class="w-16 h-16" />
             <h2 class="font-semibold">Research user needs</h2>
@@ -16,7 +16,7 @@
           </div>
         </div>
 
-        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+        <div class="bg-white rounded-md p-5 shadow-lg">
           <div class="space-y-2">
             <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon2.png' ); ?>" class="w-16 h-16" />
             <h2 class="font-semibold">Idea Generation</h2>
@@ -26,7 +26,7 @@
           </div>
         </div>
 
-        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+        <div class="bg-white rounded-md p-5 shadow-lg">
           <div class="space-y-2">
             <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon3.png' ); ?>" class="w-16 h-16" />
 
@@ -37,7 +37,7 @@
           </div>
         </div>
 
-        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+        <div class="bg-white rounded-md p-5 shadow-lg">
           <div class="space-y-2">
             <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon4.png' ); ?>" class="w-16 h-16" />
 
@@ -48,7 +48,7 @@
           </div>
         </div>
 
-        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+        <div class="bg-white rounded-md p-5 shadow-lg">
           <div class="space-y-2">
             <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon5.png' ); ?>" class="w-16 h-16" />
             <h2 class="font-semibold">Sourcing</h2>
@@ -60,7 +60,7 @@
           </div>
         </div>
 
-        <div class="w-full md:w-1/4 rounded-md p-5 shadow-lg">
+        <div class="bg-white rounded-md p-5 shadow-lg">
           <div class="space-y-2">
             <img src="<?php echo esc_url( get_template_directory_uri() . '/public/images/icon6.png' ); ?>" class="w-16 h-16" />
             <h2 class="font-semibold">Build Marketing Plan</h2>

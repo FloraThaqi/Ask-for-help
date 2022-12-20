6e268f0e5fbbf0ddd7a184fafc7a3863464307ac
denisademaku1 <73554758+denisademaku1@users.noreply.github.com>
2022-12-20 17:06:39 +0100

    Add new posttype Questions

--- wp-content/themes/starlabs/functions.php
+++ wp-content/themes/starlabs/functions.php
@@ -23,3 +23,48 @@ function style_enqueue()
     wp_enqueue_script('script', get_template_directory_uri() . '/scripts/script.js');
 }
 add_action('wp_enqueue_scripts', 'style_enqueue');
+
+/*
+	==========================================
+	 Questions Post Type
+	==========================================
+*/
+function questions_post_type (){
+	
+	$labels = array(
+		'name' => 'Questions',
+		'singular_name' => 'Questions',
+		'add_new' => 'Add Question',
+		'all_items' => 'All Questions ',
+		'add_new_item' => 'Add Question',
+		'edit_item' => 'Edit Question',
+		'new_item' => 'New Question',
+		'view_item' => 'View Question',
+		'search_item' => 'Search Questions',
+		'not_found' => 'No questions found',
+		'not_found_in_trash' => 'No questions found in trash',
+		'parent_item_colon' => 'Parent Item'
+	);
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'has_archive' => true,
+		'publicly_queryable' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'supports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'thumbnail',
+			'revisions',
+		),
+		'taxonomies' => array('category', 'post_tag'),
+		'menu_position' => 5,
+		'exclude_from_search' => false
+	);
+	register_post_type('questions',$args);
+}
+add_action('init','questions_post_type');

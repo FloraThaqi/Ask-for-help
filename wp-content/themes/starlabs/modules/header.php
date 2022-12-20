090ab3102a9f94ba12271e9acd0000f49cc0c42e
sarabajraktari <sarabajraktari90@gmail.com>
2022-12-20 13:48:12 +0100

    add css style

---
+++ wp-content/themes/starlabs/modules/header.php
@@ -0,0 +1,9 @@
+<div
+    class="class doubleDropdown hidden md:absolute bg-[#232428] text-center mx-auto rounded-lg md:mt-2 z-10 w-44 divide-y md:py-2 ">
+</div>
+
+<div class="class  md:absolute bg-[#232428] text-center mx-auto rounded-lg md:mt-2 z-10 w-44 divide-y md:py-2 "></div>
+<div class="class block p-1 text-[#F1F3F7] text-center hover:bg-gray-600 md:hover:bg-transparent"></div>
+<div class="class dropdown-menu block p-1  text-[#F1F3F7] text-center hover:bg-gray-600 md:hover:bg-transparent "></div>
+<div class="class block p-1  text-[#F1F3F7] text-center hover:bg-gray-600 "></div>
+<div class="class  text-white w-1 h-1 p-1 md:p-0.5"></div>

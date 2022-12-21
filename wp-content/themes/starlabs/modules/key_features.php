bc6d659261e9892aa4eef1b14b3c5bbbbfd2e813
FloraThaqi <florathaqi9@gmail.com>
2022-12-21 10:28:42 +0100

    Create key_features.php

---
+++ wp-content/themes/starlabs/modules/key_features.php
@@ -0,0 +1,62 @@
+<?php 
+	
+/*
+	Template Name: Key Features
+*/
+?>
+    <section class="">
+        <h1 class=" tracking-normal normal-case font-bold text-5xl text-center mt-16 mx-auto mb-0">Key Features</h1>
+
+        <p class=" mt-4 mx-auto mb-0 text-gray-400 text-center">Sample text. Click to select the text box. Click again or double click to start editing the text. Image from <a href="https://freepik.com/photos/business"  class="underline hover:no-underline p-0 ">Freepik</a></p>
+        <img class="mt-[35px] mb-0 object-center relative max-h-[500px] w-full mx-auto" src="//images03.nicepage.com/c461c07a441a5d220e8feb1a/989ba51b0f1f5d87a311be92/dawq-min.jpg?version=">
+       <div class="flex justify-center h-40">
+			<div class="absolute container grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  gap-6 -mt-[170px]  mb-[70px] max-w-[70%] ">
+				<div class="bg-white rounded-2xl py-6 px-7 shadow-lg mb-8">
+				
+						<span class=" bg-none my-0"> 
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-[59px] h-[59px] fill-none stroke-blue-700 stroke-1 mx-auto">
+							<path  d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+						</svg>
+						</span>
+					<h4 class="text-lg font-bold mt-5 mx-2.5 mb-0 text-center">Projects</h4>
+					<p class="text-5xl font-bold mt-[35px] mx-[9] mb-0 text-blue-700 text-center">100</p>
+					
+				</div>
+				<div class="bg-white rounded-2xl py-6 px-7 shadow-lg mb-8">
+						<span class=" bg-none my-0"> 
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-[59px] h-[59px] fill-none stroke-blue-700 stroke-1 mx-auto">
+							<path  d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+						</svg>
+						</span>
+					<h4 class="text-lg font-bold mt-5 mx-2.5 mb-0 text-center">Clients</h4>
+					<p class="text-5xl font-bold mt-[35px] mx-[9] mb-0 text-blue-700 text-center">80</p>
+					
+				</div>
+				<div class="bg-white rounded-2xl py-6 px-7 shadow-lg mb-8">
+						<span class=" bg-none my-0"> 
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-[59px] h-[59px] fill-none stroke-blue-700 stroke-1 mx-auto">
+							<path  d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+						</svg>
+						</span>
+					<h4 class="text-lg font-bold mt-5 mx-2.5 mb-0 text-center">6000+ Sites</h4>
+					<p class="text-5xl font-bold mt-[35px] mx-[9] mb-0 text-blue-700 text-center">6000</p>
+				
+				</div>
+				<div class="bg-white rounded-2xl py-6 px-7 shadow-lg mb-8">
+						<span class=" bg-none my-0"> 
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-[59px] h-[59px] fill-none stroke-blue-700 stroke-1 mx-auto">
+							<path  d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+						</svg>
+						</span>
+					<h4 class="text-lg font-bold mt-5 mx-2.5 mb-0 text-center">Gifts</h4>
+					<p class="text-5xl font-bold mt-[35px] mx-[9] mb-0 text-blue-700 text-center">76</p>
+				
+				</div>
+            
+
+            </div>
+        </div>      
+
+
+
+    </section>

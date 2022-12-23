37d2c370991e449c78814f8ee52050e1cbaf4f1b
ritmiraberisha <ritmiraberisha@gmail.com>
2022-12-23 16:53:56 +0100

    log in removed

--- wp-content/themes/starlabs/page-register.php
+++ wp-content/themes/starlabs/page-register.php
@@ -65,13 +65,8 @@ if ($user_ID)
             $new_user_id = wp_create_user( $username, $password, $email );   
    
             $success = 1; 
-            
-             // Log the new user in
-             wp_setcookie($username, $password, true);
-             wp_set_current_user($new_user_id, $username);	
-             do_action('wp_login', $username);
              
-             // Send the newly created user to the home page after logging them in
+             // Send the newly created user to the home page 
             wp_redirect(home_url()); exit;
    
         }  

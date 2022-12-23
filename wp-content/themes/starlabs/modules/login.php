2a69380b4a85ae2135e6736f9e8c9344a44597e5
sarabajraktari <sarabajraktari90@gmail.com>
2022-12-23 18:34:12 +0100

    loginPage

--- wp-content/themes/starlabs/modules/login.php
+++ wp-content/themes/starlabs/modules/login.php
@@ -2,51 +2,48 @@
 /* 
 Template Name: Login 
 */  
-
+ob_start();
 get_header();   
-global $wpdb, $user_ID;  
-if ($user_ID) {  
-}else
+if($_POST) 
 {  
-    $errors = array();  
+   
+    global $wpdb;  
+   
+    //We shall SQL escape all inputs  
+    $username = $wpdb->escape($_REQUEST['username']);  
+    $password = $wpdb->escape($_REQUEST['password']);  
+   
 
-    if( $_SERVER['REQUEST_METHOD'] == 'POST' ) 
-      {  
-
-        // Check username is present and not already in use  
+   
+    $login_data = array();  
+    $login_data['user_login'] = $username;  
+    $login_data['user_password'] = $password;  
+   
+    $user_verify = wp_signon( $login_data, false );   
+   
+    if ( is_wp_error($user_verify) )   
+    {  
+        
+        $errors = array();  
         $username = $wpdb->escape($_REQUEST['username']);  
-        if ( strpos($username, ' ') !== false )
-        {   
-            $errors['username'] = "Sorry, no spaces allowed in usernames";  
-        }  
-        if(empty($username)) 
-        {   
-            $errors['username'] = "Please enter a username";  
-        } elseif( username_exists( $username ) ) 
-        {  
-            $errors['username'] = "Username already exists, please try another";  
-        }  
-        // Check password is valid  
-        if(0 === preg_match("/.{6,}/", $_POST['password']))
-        {  
-          $errors['password'] = "Password must be at least six characters";  
-        }  
-
-        if(0 === count($errors)) 
-         {  
-
-            $password = $_POST['password'];  
-
-            $new_user_id = wp_create_user( $username, $password);  
-
-            $success = 1;  
-
-        }  
-
-    }  
-}  
-
-?>
+        $password=$wpdb->escape($_REQUEST['password']);
+        if(empty($username) ){
+            $errors['username']="Please enter a username";
+        }
+        elseif(empty($password)){
+            $errors['password']="Please enter a password";
+        }
+       else{
+            $errors['password']="Username or Password is incorrect!";
+        }
+     } else
+    {    
+       wp_redirect(home_url());
+       exit();  
+     }  
+   
+} 
+ ?>
 
 
 <!-- Login -->
@@ -54,26 +51,37 @@ if ($user_ID) {
     <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2 m-5">
         <div class="bg-white px-6 py-8 rounded shadow-md text-black">
             <h1 class="mb-8 text-3xl text-center">Log in</h1>
-            <form class="space-y-4 md:space-y-6" id="wp_signup_form" action="<?php echo $_SERVER['REQUEST_URI']; ?>"
+            <form class="space-y-4 md:space-y-6" id="login1" name="form" action="<?php echo home_url(); ?>/login/"
                 method="post">
                 <div>
 
                     <input type="text" class="block border border-gray-300 w-full p-3 rounded mb-4" name="username"
                         placeholder="Username" />
-
+                    <?php if(isset($errors['username'])):?>
+                    <span class="text-red-500 text-sm">
+                        <?php echo $errors['username']?>
+                    </span>
+                    <?php endif ?>
                 </div>
                 <input type="password" class="block border border-gray-300 w-full p-3 rounded mb-4" name="password"
                     placeholder="Password" />
-                    <input type="checkbox" value="lsRememberMe" id="rememberMe"> <label for="rememberMe">Remember me</label>
-                    <div class="justify-center text-center p-4 ">
-                        <button type="submit" id="submitbtn" name="submit"
-                            class="p-2 my-4 py-3 rounded-full bg-[#4767C9] text-white hover:bg-[#4767E9] focus:outline-none my-1">Login
-                        </button>
-                    </div>
-                    
+                <?php if(isset($errors['password'])):?>
+                <span class="text-red-500 text-sm">
+                    <?php echo $errors['password']?>
+                </span>
+                <?php endif ?>
+                <br>
+
+                <input type="checkbox" value="remember" id="remember"> <label for="remember">Remember me</label>
+                <div class="justify-center text-center p-4 ">
+                    <button type="submit" id="submitbtn" name="submit"
+                        class="w-full text-center py-3 rounded bg-[#4767C9] text-white hover:bg-[#4767E9] focus:outline-none my-1">Login
+                    </button>
+                </div>
+
 
                 <div class="text-center text-sm text-gray-700 mt-4">
-                By log in, you agree to the
+                    By log in, you agree to the
                     <a class="no-underline border-b border-gray-700 text-gray-700" href="#">
                         Terms of Service
                     </a> and
@@ -84,13 +92,13 @@ if ($user_ID) {
         </div>
         </form>
         <div class="text-gray-700 m-5">
-            Not registered yet? 
+            Not registered yet?
             <a class="no-underline border-b border-blue-900 text-blue-900 " href="#">
-            Create an Account
+                Create an Account
             </a>.
         </div>
 
     </div>
 </div>
-
+<?php ob_end_flush();?>
 <?php get_footer(); ?>

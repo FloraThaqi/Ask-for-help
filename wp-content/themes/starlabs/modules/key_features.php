0d31be2acd002b4b4e8fd90c6f9ba1069b2b9abe
Rinor Gerxhaliu <75761239+rinori-g@users.noreply.github.com>
2022-12-27 09:32:40 +0100

    merging

--- wp-content/themes/starlabs/modules/key_features.php
+++ wp-content/themes/starlabs/modules/key_features.php
@@ -1,4 +1,4 @@
-<?php 
+ <?php 
 /*
 	Template Name: Key Features
 */
@@ -8,11 +8,11 @@ $boxes = $module['boxes'];
 
 ?>
     <div class="w-full">
-        <h1 class=" tracking-normal normal-case font-bold text-5xl text-center mt-10 mx-auto mb-0"><?php echo $title ?></h1>
+        <h1 class=" tracking-normal normal-case font-bold text-5xl text-center mt-10 md:mt-20 mx-auto mb-0"><?php echo $title ?></h1>
         <p class=" mt-4 mx-auto mb-0 text-gray-400 text-center"><?php echo $paragraph ?> <a href="https://freepik.com/photos/business"  class="underline hover:no-underline p-0 ">Freepik</a></p>
         <img class="mt-[30px] mb-0 object-cover w-full h-[469px] mx-auto" src="//images03.nicepage.com/c461c07a441a5d220e8feb1a/989ba51b0f1f5d87a311be92/dawq-min.jpg?version=">
         <div class="w-full">
-			<div class="max-w-[1008px] m-auto max-lg:w-11/12 mb-6">
+			<div class="max-w-[1008px] m-auto max-lg:w-11/12 mb-6 md:mb-14">
 				<div class="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 container m-auto mt-[-174px] max-lg:w-full">
 				    <?php foreach($boxes as $box):?>
 				        <div class="bg-white rounded-3xl py-6 px-7 shadow-lg mb-8 w-[220px] min-h-[220px] max-lg:m-auto max-lg:w-full">
@@ -24,37 +24,6 @@ $boxes = $module['boxes'];
 						
 					    </div>
 				    <?php endforeach; ?>	  
-					<!--<div class="bg-white rounded-3xl py-6 px-7 shadow-lg mb-8 w-[220px] min-h-[220px] max-lg:m-auto max-lg:w-full">
-							<span class=" bg-none my-0">  
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-[59px] h-[59px] fill-none stroke-blue-700 stroke-1 mx-auto">
-								<path  d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
-							</svg>
-							</span>
-						<h4 class="text-lg font-bold mt-5 mx-2.5 mb-0 text-center">Clients</h4>
-						<p class="text-5xl font-bold mt-[35px] mx-[9] mb-0 text-blue-700 text-center">80</p>
-						
-					</div>
-					<div class="bg-white rounded-3xl py-6 px-7 shadow-lg mb-8 w-[220px] min-h-[220px] max-lg:m-auto max-lg:w-full">
-							<span class=" bg-none my-0"> 
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-[59px] h-[59px] fill-none stroke-blue-700 stroke-1 mx-auto">
-								<path  d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
-							</svg>
-							</span>
-						<h4 class="text-lg font-bold mt-5 mx-2.5 mb-0 text-center">6000+ Sites</h4>
-						<p class="text-5xl font-bold mt-[35px] mx-[9] mb-0 text-blue-700 text-center">6000</p>
-					
-					</div>
-					<div class="bg-white rounded-3xl py-6 px-7 shadow-lg mb-8 w-[220px] min-h-[220px] max-lg:m-auto max-lg:w-full">
-							<span class=" bg-none my-0"> 
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-[59px] h-[59px] fill-none stroke-blue-700 stroke-1 mx-auto">
-								<path  d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
-							</svg>
-							</span>
-						<h4 class="text-lg font-bold mt-5 mx-2.5 mb-0 text-center">Gifts</h4>
-						<p class="text-5xl font-bold mt-[35px] mx-[9] mb-0 text-blue-700 text-center">76</p>
-					
-					</div>-->
-					
 				</div>
 			</div>
         </div>      

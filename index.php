2752f2f8a910a8b63b6f7b63215401cf8532b825
edi <114887014+edishabani@users.noreply.github.com>
2022-12-29 11:58:06 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+<?php get_header();?>
+<?php 
+   $field = get_field('modules');
+   if (is_array($field)) {
+        foreach ($field as $key => $value) {
+            $module = $value[$value['acf_fc_layout']];
+            include (get_template_directory().'/modules/'.$value['acf_fc_layout'].'.php');
+        }
+   }
+get_footer();?>

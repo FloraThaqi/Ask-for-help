11e326ab4e5a93a3e8578de312d14039b64cb814
ardialiqkaj <ardi.aliqkaj@gmail.com>
2022-12-30 14:20:11 +0100

    first design commit

--- wp-content/themes/starlabs/modules/display_posts_category.php
+++ wp-content/themes/starlabs/modules/display_posts_category.php
@@ -8,30 +8,36 @@ $byDefault_relation = $module['by_default_relation'];
 <?php
 $args = array(
     'cat' => $byDefault_relation
-);
-
-if ($category_selection == 'By default') {
-    $lastBlog = new WP_Query($args);
-
-    if ($lastBlog->have_posts()) :
-
-        while ($lastBlog->have_posts()) : $lastBlog->the_post(); ?>
-
-            <?php the_title();
-            the_content(); ?>
-
-        <?php endwhile;
-
-    endif;
-
-    wp_reset_postdata();
-} else {
-    foreach ($category_relation as $value) : ?>
-
-        <h2><?php echo $value->post_title; ?></h2>
-        <p><?php echo $value->post_content; ?></p>
-
-<?php endforeach;
-}
-
-?>
+); ?>
+<section class="bg-white">
+    <?php if ($category_selection == 'By default') {
+        $lastBlog = new WP_Query($args); ?>
+        <div class="max-w-[1008px] m-auto my-10 max-lg:mx-4">
+            <?php if ($lastBlog->have_posts()) : ?>
+                <div class="grid grid-cols-3 gap-8 max-lg:grid-cols-2 max-sm:grid-cols-1 py-8">
+                    <?php while ($lastBlog->have_posts()) : $lastBlog->the_post(); ?>
+                        <div class="text-gray-500">
+                            <h2 class="text-center mb-2 text-gray-600 font-bold"><?php the_title(); ?></h2>
+                            <p class=""><?php the_content(); ?></p>
+                            <p class="pt-3"><?php _e('Posted on: ', 'textdomain') . the_time('F j, Y g:i a'); ?></p>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    <?php wp_reset_postdata();
+    } else { ?>
+        <div class="max-w-[1008px] m-auto my-10 max-lg:mx-4">
+            <div class="grid grid-cols-3 gap-8 max-lg:grid-cols-2 max-sm:grid-cols-1 py-8">
+                <?php foreach ($category_relation as $value) : ?>
+                    <div class="text-gray-500">
+                        <h2 class="text-center mb-2 text-gray-600 font-bold"><?php echo $value->post_title; ?></h2>
+                        <p class=""><?php echo $value->post_content; ?></p>
+                        <p class="pt-3"><?php echo $value->post_date; ?></p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    <?php }
+    ?>
+</section>

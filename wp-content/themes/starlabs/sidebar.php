009978bfb1ec577efd886007434d56972f0d9a3f
denisademaku1 <73554758+denisademaku1@users.noreply.github.com>
2023-01-06 12:59:11 +0100

    Sidebar

--- wp-content/themes/starlabs/sidebar.php
+++ wp-content/themes/starlabs/sidebar.php
@@ -15,19 +15,26 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <aside id="secondary" class="widget-area">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
-
 <?php
 $title = get_field('title','option');
 $pages = get_field('pages','option');?>
 
-<div class="sidebar bg-gray-100 p-4 rounded-lg shadow-lg sticky top-0 h-screen">
-  <h3 class="font-bold text-xl mb-4"><?php echo $title;?></h3>
-	<?php foreach ( $pages as $page ) :?>
-  <ul class="block py-2 px-4 rounded-lg text-blue-500 hover:bg-blue-500 hover:text-white">
-		<?php
-			echo ($page['link']['title']);
-		endforeach;
 
-				?>
-  </ul>
-</div>
+
+<aside class="box-shadow bg-gray-900 h-inherit rounded-t-lg w-90 lg:gap-2 sticky top-0">
+    <div class="flex justify-center w-full">
+      <h1 class="text-xl bg-gray-600 rounded-3xl px-4 font-serif font-semibold tracking-widest uppercase text-[#ffffff] my-4"><?php echo $title;?></h1>
+    </div>
+    <div class="my-2 bg-white h-[1px]"></div>
+    <div class="flex flex-wrap bg-gray-900 lg:w-full pr-8 pl-3">
+        <ul class="block">
+        <?php foreach ( $pages as $page ) :?>
+            <li class="hover:text-[#4767c9] text-white  font-serif  mb-2">
+                <a href="<?php echo $page['link']['url'];?>"><?php echo $page['link']['title'];?></a> 
+            <?php
+            endforeach;
+                    ?>
+            </li>
+        </ul>
+    </div>
+</aside>

62b2be44eabdcd9b1c8e87fa2bed26e3030e8ab0
ardialiqkaj <ardi.aliqkaj@gmail.com>
2023-01-11 12:16:21 +0100

    fixed styling with sidebar

--- wp-content/themes/starlabs/modules/display_posts_category.php
+++ wp-content/themes/starlabs/modules/display_posts_category.php
@@ -20,11 +20,11 @@ $args = array(
 <section class="">
     <?php if ($category_selection == 'By default') {
         $lastBlog = new WP_Query($args); ?>
-        <div class="w-full m-auto  max-lg:mx-4">
+        <div class="w-full m-auto max-lg:mx-4">
             <?php if ($lastBlog->have_posts()) : ?>
-                <div class="w-full  m-auto my-8">
+                <div class="max-w-[700px] m-auto py-8">
                     <?php while ($lastBlog->have_posts()) : $lastBlog->the_post(); ?>
-                        <div class="border-y-[1px] border-x-[0.5px] border-gray-200 bg-white border-collapse p-4 mb-3">
+                        <div class="border-y-[1px] border-x-[0.5px] border-gray-200 border-collapse p-4 mb-3">
                             <?php
                             $title_variable = get_field('question_title');
                             $description_variable = get_field('question_description');
@@ -37,7 +37,10 @@ $args = array(
                             </div>
                             <div class="text-gray-500 w-full m-auto my-2">
                                 <h2 class="mb-2 text-gray-800 font-bold"><?php echo $title_variable; ?></h2>
-                                <p class=""><?php echo $description_variable; ?></p>
+                                <p class="">
+                                <p class=""><?php $desc_string = strval($description_variable);
+                                            echo substr($desc_string, 0, 200); ?><b> . . .</b></p>
+                                </p>
                             </div>
                             <div class="flex justify-end  min-h-[40px] items-center w-full mx-auto">
                                 <a class="min-w-[80px] h-[35px] bg-black text-white flex justify-center items-center mr-3 rounded" href="<?php echo the_permalink(); ?>">Answer</a>
@@ -49,10 +52,10 @@ $args = array(
         </div>
     <?php wp_reset_postdata();
     } else { ?>
-        <div class="max-w-[1008px] m-auto max-lg:mx-4">
+        <div class="w-full m-auto max-lg:mx-4">
             <div class="max-w-[700px] m-auto py-8">
                 <?php foreach ($category_relation as $value) : ?>
-                    <div class="border-y-[1px] border-x-[0.5px] border-gray-200 border-collapse p-4">
+                    <div class="border-y-[1px] border-x-[0.5px] border-gray-200 border-collapse p-4 mb-3">
                         <?php
                         $title_variable = get_field('question_title', $value->ID);
                         $description_variable = get_field('question_description', $value->ID);
@@ -62,14 +65,17 @@ $args = array(
                         ?>
                         <div class="flex max-md:justify-between">
                             <img class="w-8 h-8 rounded-3xl mr-2 border-sky-600 border-2 p-[1px]" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="user profile">
-                            <p class="text-gray-500 mr-2">Asked on: <?php echo $date_variable; ?> | </p>
-                            <a class="text-gray-500">In: <?php echo $getslug ?></a>
+                            <p class="text-gray-500 leading-8 mr-2">Asked on: <?php echo $date_variable; ?> | </p>
+                            <a class="text-gray-500 leading-8">In: <?php echo $getslug ?></a>
                         </div>
-                        <div class="text-gray-500 max-w-[620px] m-auto my-2">
+                        <div class="text-gray-500 w-full m-auto my-2">
                             <h2 class="mb-2 text-gray-800 font-bold"><?php echo $title_variable; ?></h2>
-                            <p class=""><?php echo $description_variable; ?></p>
+                            <p class="">
+                            <p class=""><?php $desc_string = strval($description_variable);
+                                        echo substr($desc_string, 0, 200); ?><b> . . .</b></p>
+                            </p>
                         </div>
-                        <div class="flex justify-end bg-gray-200 min-h-[60px] items-center max-w-[620px] mx-auto">
+                        <div class="flex justify-end  min-h-[40px] items-center w-full mx-auto">
                             <a class="min-w-[80px] h-[35px] bg-black text-white flex justify-center items-center mr-3 rounded" href="<?php echo the_permalink($value->ID); ?>">Answer</a>
                         </div>
                     </div>

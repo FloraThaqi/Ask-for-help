a7acbd45e6f1b5cf73d0c88e76f3eab04d48d608
sarabajraktari <sarabajraktari90@gmail.com>
2023-01-11 14:37:48 +0100

    burger menu

--- wp-content/themes/starlabs/header.php
+++ wp-content/themes/starlabs/header.php
@@ -28,10 +28,10 @@
 
             <!-- Burger Menu -->
             <button type="button"
-                class="mobile-menu-button inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200  "
+                class="mobile-menu-button inline-flex items-center p-2 ml-3 text-sm   rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200  "
                 aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
-                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                <svg class="w-8 h-8 " aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg" x-show="!showMenu">
                     <path fill-rule="evenodd"
                         d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
--- wp-content/themes/starlabs/modules/header.php
+++ wp-content/themes/starlabs/modules/header.php
@@ -2,7 +2,8 @@
     class="class doubleDropdown hidden md:absolute bg-[#232428] text-center mx-auto rounded-lg md:mt-2 z-10 w-44 divide-y md:py-2 ">
 </div>
 
-<div class="class  md:absolute bg-[#232428] text-center mx-auto rounded-lg md:mt-2 z-10 w-44 divide-y md:py-2 "></div>
+<div class="class  md:absolute  bg-[#34ccfc] text-center mx-auto rounded-lg md:mt-2 z-10 w-44 divide-y md:py-2 ">
+</div>
 <div class="class block p-1 text-[#F1F3F7] text-center hover:bg-gray-600 md:hover:bg-transparent"></div>
 <div class="class dropdown-menu block p-1  text-[#F1F3F7] text-center hover:bg-gray-600 md:hover:bg-transparent "></div>
 <div class="class block p-1  text-[#F1F3F7] text-center hover:bg-gray-600 "></div>

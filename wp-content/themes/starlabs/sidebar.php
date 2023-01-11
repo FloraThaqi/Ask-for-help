45e0a2b472b964d11a1f54ce8c298932ccc5d6b5
ritmiraberisha <ritmiraberisha@gmail.com>
2023-01-11 12:06:03 +0100

    font-style

--- wp-content/themes/starlabs/sidebar.php
+++ wp-content/themes/starlabs/sidebar.php
@@ -22,12 +22,12 @@ $pages = get_field('pages','option');?>
 
 
 <aside class="box-shadow shadow-lg bg-slate-200	 h-inherit rounded-b-lg w-90 lg:gap-2 ml-6 sticky top-0">
-      <h1 class="text-base px-4 font-serif font-semibold tracking-widest uppercase text-gray-600 py-4"><?php echo $title;?></h1>
+      <h1 class="text-base px-4 font-semibold tracking-widest uppercase text-gray-600 py-4"><?php echo $title;?></h1>
     <div class="bg-slate-400	 h-[1px]"></div>
     <div class="flex flex-col rounded-lg bg-slate-200 lg:w-full ">
         <ul class="block"> 
         <?php foreach ( $pages as $page ) :?>
-            <li class="hover:text-[#4767c9] mx-5 text-black font-serif py-4 border-b border-slate-300 last:border-none">
+            <li class="hover:text-[#4767c9] mx-5 text-black py-4 border-b border-slate-300 last:border-none">
                 <a href="<?php echo $page['link']['url'];?>" class=" "><?php echo $page['link']['title'];?></a> 
             <?php
             endforeach;

eec0ef036557a322dec102a8b6defaf0921f3172
Rinor Gerxhaliu <rinor.gerxhaliu@catenamedia.com>
2023-01-12 11:01:28 +0100

    logout

--- wp-content/themes/starlabs/functions.php
+++ wp-content/themes/starlabs/functions.php
@@ -81,7 +81,24 @@ function questions_post_type (){
 }
 add_action('init','questions_post_type');
 
-
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2); 
+function logout_without_confirm($action, $result) 
+{ 
+    /** 
+     * Allow logout without confirmation 
+     */ 
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) { 
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : ''; 
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));; 
+        header("Location: $location"); 
+        die; 
+    } 
+}
+add_action('wp_logout','ps_redirect_after_logout');
+function ps_redirect_after_logout(){
+         wp_redirect( home_url() );
+         exit();
+}
 
 /*
 	==========================================

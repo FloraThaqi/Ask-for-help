1f83ca21d82a0200a839722edeb8449e495808fe
denisademaku1 <73554758+denisademaku1@users.noreply.github.com>
2023-01-16 15:27:55 +0100

    Automatically add categories to sidebar

--- wp-content/themes/starlabs/sidebar.php
+++ wp-content/themes/starlabs/sidebar.php
@@ -21,18 +21,16 @@ $pages = get_field('pages','option');?>
 
 
 
+
 <aside class="box-shadow shadow-lg bg-slate-200	 h-inherit rounded-b-lg w-90 lg:gap-2 ml-6 sticky top-0">
-    <h1 class="text-base px-4 font-semibold tracking-widest uppercase text-gray-600 py-4"><?php echo $title;?></h1>
-    <div class="bg-slate-400	 h-[1px]"></div>
-    <div class="flex flex-col rounded-lg bg-slate-200 lg:w-full ">
-        <ul class="block">
-            <?php foreach ( $pages as $page ) :?>
-            <li class="hover:text-[#4767c9] mx-5 text-black py-4 border-b border-slate-300 last:border-none">
-                <a href="<?php echo $page['link']['url'];?>" class=" "><?php echo $page['link']['title'];?></a>
-                <?php
-            endforeach;
-                    ?>
-            </li>
-        </ul>
-    </div>
+<h2 class="text-base px-4 font-semibold tracking-widest uppercase text-gray-600 py-4">Categories</h2>
+  <ul class="text-sm font-medium">
+    <?php wp_get_archives( array(
+      'type' => 'postbypost',
+      'limit' => 10,
+      'before' => '<li class="hover:text-[#4767c9] mx-5 text-black py-4 border-b border-slate-300 last:border-none">',
+      'after' => '</li>'
+    ) ); ?>
+  </ul>
 </aside>
+

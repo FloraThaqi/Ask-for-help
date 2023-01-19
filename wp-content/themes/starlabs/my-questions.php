c9a554e70857276e2a81665c9b3aab210b41a5f6
ritmiraberisha <ritmiraberisha@gmail.com>
2023-01-19 14:42:20 +0100

    fixed

--- wp-content/themes/starlabs/my-questions.php
+++ wp-content/themes/starlabs/my-questions.php
@@ -33,7 +33,8 @@ Template Name: My Questions
                                 $date_variable = get_field('question_date');
                                 $terms = get_the_terms( get_the_ID(), 'field' );
                                 if ( $terms && ! is_wp_error( $terms ) ) : 
-                                $cat_name = $terms[0]->name;
+                                $cat_name = $terms[0]->name; else :
+                                    $cat_name = 'N/A';
                                 endif;
                             ?>
                                 <div class="border-y-[1px] border-x-[0.5px] bg-white border-gray-200 border-collapse p-4 mb-3">

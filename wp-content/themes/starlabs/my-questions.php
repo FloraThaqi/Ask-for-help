df832208348aff005a6c504b5a194cf250df4576
sarabajraktari <sarabajraktari90@gmail.com>
2023-01-25 09:42:14 +0100

    solve question

--- wp-content/themes/starlabs/my-questions.php
+++ wp-content/themes/starlabs/my-questions.php
@@ -33,6 +33,7 @@ Template Name: My Questions
                                 $title_variable = get_field('question_title');
                                 $description_variable = get_field('question_description');
                                 $date_variable = get_field('question_date');
+                                $close=get_field('close');
                                 $terms = get_the_terms( get_the_ID(), 'field' );
                                 gt_set_post_view();
                                 if ( $terms && ! is_wp_error( $terms ) ) : 
@@ -42,13 +43,43 @@ Template Name: My Questions
                             ?>
                             <div
                                 class="border-y-[1px] border-x-[0.5px] bg-white border-gray-200 border-collapse p-4 mb-3">
-                                <div class="flex max-md:justify-between">
+                                <div class="flex max-md:justify-between relative">
                                     <img class="w-8 h-8 rounded-3xl mr-2 border-sky-600 border-2 p-[1px]"
                                         src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
                                         alt="user profile">
                                     <p class="text-gray-500 leading-8 mr-2">Asked on: <?php echo $date_variable; ?> |
                                     </p>
                                     <a class="text-gray-500 leading-8">In: <?php echo $cat_name ?></a>
+
+
+
+                                    <div class="absolute top-0 right-0">
+
+                                        <div class="">
+                                            <?php if(!$close) :?>
+                                            <button type="button" id="button1" name="button1"
+                                                class="bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none ">
+                                                <p class=" text-slate-500 text-lg">Mark as solved</p>
+
+                                            </button>
+                                            <?php
+                    
+                                        ?>
+                                            <?php else  :?>
+
+
+                                            <div class="w-16  overflow-hidden inline-block relative">
+                                                <div class=" h-8  bg-green-600 -rotate-45 ">
+                                                </div>
+                                                <div>
+                                                    <p class=" text-black font-bold text-lg absolute top-0 ">Solved </p>
+                                                </div>
+                                            </div>
+
+
+                                            <?php endif ;?>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="text-gray-500 w-full m-auto my-2">
                                     <h2 class="mb-2 text-gray-800 font-bold"><?php echo $title_variable; ?></h2>
@@ -56,14 +87,18 @@ Template Name: My Questions
                                         <?php $desc_string = strval($description_variable);
                                                     echo substr($desc_string, 0, 200); ?><b> . . .</b>
                                     </p>
+
                                 </div>
                                 <div class="flex">
                                     <div class=" flex text-xs py-1">
-                                        <svg class="w-4 h-4 mr-2 fill-slate-500" xmlns="http://www.w3.org/2000/svg " viewBox="0 0 576 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                                            <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM432 256c0 79.5-64.5 144-144 144s-144-64.5-144-144s64.5-144 144-144s144 64.5 144 144zM288 192c0 35.3-28.7 64-64 64c-11.5 0-22.3-3-31.6-8.4c-.2 2.8-.4 5.5-.4 8.4c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-2.8 0-5.6 .1-8.4 .4c5.3 9.3 8.4 20.1 8.4 31.6z" />
+                                        <svg class="w-4 h-4 mr-2 fill-slate-500" xmlns="http://www.w3.org/2000/svg "
+                                            viewBox="0 0 576 512">
+                                            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                                            <path
+                                                d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM432 256c0 79.5-64.5 144-144 144s-144-64.5-144-144s64.5-144 144-144s144 64.5 144 144zM288 192c0 35.3-28.7 64-64 64c-11.5 0-22.3-3-31.6-8.4c-.2 2.8-.4 5.5-.4 8.4c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-2.8 0-5.6 .1-8.4 .4c5.3 9.3 8.4 20.1 8.4 31.6z" />
                                         </svg>
-                                        <span class="min-w-[60px]"><?= gt_get_post_view(); ?></span>  
-                                    </div>   
+                                        <span class="min-w-[60px]"><?= gt_get_post_view(); ?></span>
+                                    </div>
                                     <div class="flex justify-end  min-h-[40px] items-center w-full mx-auto">
                                         <a class="min-w-[80px] h-[35px] bg-indigo-500 text-white flex justify-center items-center mr-3 rounded"
                                             href="<?php echo the_permalink(); ?>">View</a>
@@ -75,9 +110,10 @@ Template Name: My Questions
                                             class="hidden fixed top-0 left-0 w-full h-full flex items-center justify-center">
                                             <div class="bg-white p-6 rounded">
                                                 <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto" fill="none"
-                                                    viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto"
+                                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        stroke-width="2"
                                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
                                                     </path>
                                                 </svg>
@@ -88,7 +124,8 @@ Template Name: My Questions
                                                         d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                                                         clip-rule="evenodd" />
                                                 </svg>
-                                                <p class="text-lg mb-4">Are you sure you want to delete this question?</p>
+                                                <p class="text-lg mb-4">Are you sure you want to delete this question?
+                                                </p>
                                                 <div class="p-3  mt-2 text-center space-x-4 md:block">
                                                     <button class="bg-gray-500 text-white p-2 rounded"
                                                         onClick="hideModal()">Cancel</button>
@@ -99,7 +136,7 @@ Template Name: My Questions
                                         </div>
 
                                     </div>
-                                </div>    
+                                </div>
                             </div>
                             <?php endwhile; ?>
                         </div>

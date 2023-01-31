3618d830425f95e2ed9697d2b63d734f150300db
sarabajraktari <sarabajraktari90@gmail.com>
2023-01-31 11:46:42 +0100

    search

--- wp-content/themes/starlabs/modules/header.php
+++ wp-content/themes/starlabs/modules/header.php
@@ -10,3 +10,23 @@
 <div
     class="class text-base absolute p-6 bottom-0 left-0 text-red-600  font-display text-white w-1 h-1 p-1 md:p-0.5 hover:bg-gray-200 px-3 ml-0.5  md:min-w-[150px]">
 </div>
+
+<div class="relative mr-6 my-2">
+    <input type="search" class="bg-purple-white shadow rounded border-0 p-3" placeholder="Search by name...">
+    <div class="absolute pin-r pin-t mt-3 mr-4 text-purple-lighter">
+        <svg version="1.1" class="h-4 text-dark" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 52.966 52.966"
+            style="enable-background:new 0 0 52.966 52.966;" xml:space="preserve">
+            <path d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
+        c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
+        C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
+        S32.459,40,21.983,40z" />
+
+        </svg>
+
+    </div>
+</div>
+
+
+<input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" type="search"
+    name="search" placeholder="Search">
--- wp-content/themes/starlabs/my-questions.php
+++ wp-content/themes/starlabs/my-questions.php
@@ -28,8 +28,8 @@ get_header();
 
                         <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="modal-id-backdrop"></div>
                         <?php if ($lastBlog->have_posts()) : ?>
-                            <div class="w-full m-auto py-8">
-                                <?php while ($lastBlog->have_posts()) : $lastBlog->the_post();
+                        <div class="w-full m-auto py-8">
+                            <?php while ($lastBlog->have_posts()) : $lastBlog->the_post();
                                     $title_variable = get_field('question_title');
                                     $description_variable = get_field('question_description');
                                     $date_variable = get_field('question_date');
@@ -42,30 +42,35 @@ get_header();
                                     else :
                                         $cat_name = 'N/A';
                                     endif; ?>
-                                    <div class="border-y-[1px] border-x-[0.5px] bg-white border-gray-200 border-collapse p-4 mb-3">
-                                        <div class="flex max-md:justify-between relative">
-                                            <img class="w-8 h-8 rounded-3xl mr-2 border-sky-600 border-2 p-[1px]" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="user profile">
-                                            <p class="text-gray-500 leading-8 mr-2">Asked on: <?php echo $date_variable; ?> |
-                                            </p>
-                                            <a class="text-gray-500 leading-8">In: <?php echo $cat_name ?></a>
-                                            <div class="absolute top-0 right-0">
-                                                <div class="">
-                                                    <?php
+                            <div
+                                class="border-y-[1px] border-x-[0.5px] bg-white border-gray-200 border-collapse p-4 mb-3">
+                                <div class="flex max-md:justify-between relative">
+                                    <img class="w-8 h-8 rounded-3xl mr-2 border-sky-600 border-2 p-[1px]"
+                                        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                                        alt="user profile">
+                                    <p class="text-gray-500 leading-8 mr-2">Asked on: <?php echo $date_variable; ?> |
+                                    </p>
+                                    <a class="text-gray-500 leading-8">In: <?php echo $cat_name ?></a>
+                                    <div class="absolute top-0 right-0">
+                                        <div class="">
+                                            <?php
                                                     global $user_ID;
                                                     $post_id = get_the_ID();
                                                     $author_id = get_post_field('post_author', $post_id);
 
                                                     if (!$close) : ?>
-                                                        <?php if ($author_id == $user_ID) : ?>
-                                                            <form action="" method="POST">
+                                            <?php if ($author_id == $user_ID) : ?>
+                                            <form action="" method="POST">
 
-                                                                <button type="submit" id=<?php echo $post_ID; ?> name=<?php echo $post_ID; ?> class="bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none ">
-                                                                    <p class=" text-slate-500 text-lg">Mark as solved</p>
-                                                                </button>
-                                                            </form>
-                                                        <?php else : ?>
-                                                        <?php endif; ?>
-                                                        <?php
+                                                <button type="submit" id=<?php echo $post_ID; ?>
+                                                    name=<?php echo $post_ID; ?>
+                                                    class="bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none ">
+                                                    <p class=" text-slate-500 text-lg">Mark as solved</p>
+                                                </button>
+                                            </form>
+                                            <?php else : ?>
+                                            <?php endif; ?>
+                                            <?php
                                                         if (isset($_POST[$post_ID])) {
 
                                                             update_field('close', 1, $post_ID);
@@ -74,67 +79,82 @@ get_header();
                                                             </script>";
                                                         }
                                                         ?>
-                                                    <?php else : ?>
-                                                        <div class="w-16  overflow-hidden inline-block relative">
-                                                            <div class=" h-8  bg-green-600 -rotate-45 ">
-                                                            </div>
-                                                            <div>
-                                                                <p class=" text-black font-bold text-lg absolute top-0 ">Solved </p>
-                                                            </div>
-                                                        </div>
-                                                    <?php endif; ?>
+                                            <?php else : ?>
+                                            <div class="w-16  overflow-hidden inline-block relative">
+                                                <div class=" h-8  bg-green-600 -rotate-45 ">
+                                                </div>
+                                                <div>
+                                                    <p class=" text-black font-bold text-lg absolute top-0 ">Solved </p>
                                                 </div>
                                             </div>
+                                            <?php endif; ?>
                                         </div>
-                                        <div class="text-gray-500 w-full m-auto my-2">
-                                            <h2 class="mb-2 text-gray-800 font-bold"><?php echo $title_variable; ?></h2>
-                                            <p class="">
-                                                <?php $desc_string = strval($description_variable);
+                                    </div>
+                                </div>
+                                <div class="text-gray-500 w-full m-auto my-2">
+                                    <h2 class="mb-2 text-gray-800 font-bold"><?php echo $title_variable; ?></h2>
+                                    <p class="">
+                                        <?php $desc_string = strval($description_variable);
                                                 echo substr($desc_string, 0, 200); ?><b> . . .</b>
-                                            </p>
+                                    </p>
 
-                                        </div>
-                                        <div class="flex">
-                                            <div class="flex flex-row text-xs ">
-                                                <svg class="w-4 h-4 fill-slate-500 leading-8 mt-2 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                                                    <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                                                    <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM432 256c0 79.5-64.5 144-144 144s-144-64.5-144-144s64.5-144 144-144s144 64.5 144 144zM288 192c0 35.3-28.7 64-64 64c-11.5 0-22.3-3-31.6-8.4c-.2 2.8-.4 5.5-.4 8.4c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-2.8 0-5.6 .1-8.4 .4c5.3 9.3 8.4 20.1 8.4 31.6z" />
+                                </div>
+                                <div class="flex">
+                                    <div class="flex flex-row text-xs ">
+                                        <svg class="w-4 h-4 fill-slate-500 leading-8 mt-2 mr-2"
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                                            <path
+                                                d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM432 256c0 79.5-64.5 144-144 144s-144-64.5-144-144s64.5-144 144-144s144 64.5 144 144zM288 192c0 35.3-28.7 64-64 64c-11.5 0-22.3-3-31.6-8.4c-.2 2.8-.4 5.5-.4 8.4c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-2.8 0-5.6 .1-8.4 .4c5.3 9.3 8.4 20.1 8.4 31.6z" />
+                                        </svg>
+                                        <?php if (gt_get_post_views($post_ID) !== ' views') { ?>
+                                        <p class="text-gray-500 leading-8 mr-2 min-w-[60px]">
+                                            <?= gt_get_post_views($post_ID); ?></p>
+                                        <?php } else { ?>
+                                        <p class="text-gray-500 leading-8 mr-2 min-w-[60px]">0 Views</p>
+                                        <?php }; ?>
+                                    </div>
+
+                                    <div class="flex justify-end  min-h-[40px] items-center w-full mx-auto">
+                                        <a class="min-w-[80px] h-[35px] bg-indigo-500 text-white flex justify-center items-center mr-3 rounded"
+                                            href="<?php echo the_permalink(); ?>">View</a>
+                                        <a class="min-w-[80px] h-[35px] bg-red-500 text-white flex justify-center items-center mr-3 rounded"
+                                            href="#" onClick="showModal()">Delete</a>
+
+
+                                        <div id="deleteModal"
+                                            class="hidden fixed top-0 left-0 w-full h-full flex items-center justify-center">
+                                            <div class="bg-white p-6 rounded">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto"
+                                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        stroke-width="2"
+                                                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
+                                                    </path>
                                                 </svg>
-                                                <?php if (gt_get_post_views($post_ID) !== ' views') { ?>
-                                                    <p class="text-gray-500 leading-8 mr-2 min-w-[60px]">
-                                                        <?= gt_get_post_views($post_ID); ?></p>
-                                                <?php } else { ?>
-                                                    <p class="text-gray-500 leading-8 mr-2 min-w-[60px]">0 Views</p>
-                                                <?php }; ?>
-                                            </div>
-
-                                            <div class="flex justify-end  min-h-[40px] items-center w-full mx-auto">
-                                                <a class="min-w-[80px] h-[35px] bg-indigo-500 text-white flex justify-center items-center mr-3 rounded" href="<?php echo the_permalink(); ?>">View</a>
-                                                <a class="min-w-[80px] h-[35px] bg-red-500 text-white flex justify-center items-center mr-3 rounded" href="#" onClick="showModal()">Delete</a>
-
-
-                                                <div id="deleteModal" class="hidden fixed top-0 left-0 w-full h-full flex items-center justify-center">
-                                                    <div class="bg-white p-6 rounded">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
-                                                            </path>
-                                                        </svg>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 flex items-center text-red-500 mx-auto" viewBox="0 0 20 20" fill="currentColor">
-                                                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                                        </svg>
-                                                        <p class="text-lg mb-4">Are you sure you want to delete this question?
-                                                        </p>
-                                                        <div class="p-3  mt-2 text-center space-x-4 md:block">
-                                                            <button class="bg-gray-500 text-white p-2 rounded" onClick="hideModal()">Cancel</button>
-                                                            <a class="bg-red-500 text-white p-2 rounded" href="<?php echo get_delete_post_link(get_the_ID()); ?>">Delete</a>
-                                                        </div>
-                                                    </div>
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    class="w-16 h-16 flex items-center text-red-500 mx-auto"
+                                                    viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd"
+                                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
+                                                <p class="text-lg mb-4">Are you sure you want to delete this question?
+                                                </p>
+                                                <div class="p-3  mt-2 text-center space-x-4 md:block">
+                                                    <button class="bg-gray-500 text-white p-2 rounded"
+                                                        onClick="hideModal()">Cancel</button>
+                                                    <a class="bg-red-500 text-white p-2 rounded"
+                                                        href="<?php echo get_delete_post_link(get_the_ID()); ?>">Delete</a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                <?php endwhile; ?>
+                                </div>
                             </div>
+                            <?php endwhile; ?>
+                        </div>
                         <?php endif; ?>
                     </div>
                     <!-- Pagination -->
@@ -173,17 +193,17 @@ get_header();
 
 
 <script>
-    function showModal() {
-        document.getElementById("deleteModal").classList.remove("hidden");
-    }
+function showModal() {
+    document.getElementById("deleteModal").classList.remove("hidden");
+}
 
-    function hideModal() {
-        document.getElementById("deleteModal").classList.add("hidden");
-    }
+function hideModal() {
+    document.getElementById("deleteModal").classList.add("hidden");
+}
 
 
-    function deletePost() {
+function deletePost() {
 
-        hideModal();
-    }
+    hideModal();
+}
 </script>

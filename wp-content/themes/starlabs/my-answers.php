1bf1af02727ab90d8420fa3c1a19b6e8c1921444
denisademaku1 <73554758+denisademaku1@users.noreply.github.com>
2023-02-01 19:38:55 +0100

    fixed-some-issues-my-answers

--- wp-content/themes/starlabs/my-answers.php
+++ wp-content/themes/starlabs/my-answers.php
@@ -58,44 +58,38 @@ if(isset($_POST['update'])) {
                 <input type="submit" name="edit" value="Edit" class="min-w-[80px] h-[35px] bg-blue-500 text-white flex justify-center items-center mr-3 rounded">
               </form>
               <a href="'.get_permalink($comment->comment_post_ID).'" class="min-w-[80px] h-[35px] bg-indigo-400 text-white flex justify-center items-center mr-3 rounded">View</a>
-             
-        <button class="bg-red-500 text-white p-2 rounded" onClick="showModal()">Delete</button>
+            
+              <a class="btn-delete min-w-[80px] h-[35px] bg-red-500 text-white flex justify-center items-center mr-3 rounded"
+              href="#" data-item-id="'.$comment->comment_ID.'">Delete</a>
 
-<div id="deleteModal" class="hidden fixed top-0 left-0 w-full h-full flex items-center drop-shadow-2xl justify-center">
-  <div class="bg-white p-6 rounded">
-    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
-      </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 flex items-center text-red-500 mx-auto" viewBox="0 0 20 20" fill="currentColor">
-      <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-    </svg>
-    <p class="text-lg mb-4">Are you sure you want to delete this question?
-    </p>
-    <div class="p-3  mt-2 text-center space-x-4 md:block">
-    <form action="" method="post">
-    <input type="hidden" name="comment_id" value="'.$comment->comment_ID.'">
-    <input type="submit" name="delete" value="Delete" class="bg-red-500 text-white p-2 rounded">
-    <button class="bg-gray-500 text-white p-2 rounded" onClick="hideModal()">Cancel</button>
-    </form>
-    </div>
-  </div>
-</div>
 
-<script>
-  function showModal() {
-    document.getElementById("deleteModal").classList.remove("hidden");
-  }
-  function hideModal() {
-    document.getElementById("deleteModal").classList.add("hidden");
-  }
-</script>
+              <div id="deleteModal-'.$comment->comment_ID.'" class="hidden fixed top-0 left-0 w-full h-full flex items-center drop-shadow-2xl justify-center">
+                <div class="bg-white p-6 rounded">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
+                    </path>
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 flex items-center text-red-500 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                  </svg>
+                  <p class="text-lg mb-4">Are you sure you want to delete this comment?
+                  </p>
+                  <div class="p-3  mt-2 text-center space-x-4 md:block">
+                  <form action="" method="post">
+                  <input type="hidden" name="comment_id" value="'.$comment->comment_ID.'">
+                  <input type="submit" name="delete" value="Delete" class="bg-red-500 text-white p-2 rounded cursor-pointer">
+                  <button type="button" class="bg-gray-500 text-white p-2 rounded" onClick="hideModal('.$comment->comment_ID.')">Cancel</button>
+                  </form>
+                  </div>
+                </div>
+              </div>
+
+
             </div>
           </div>';
     }
   }
   ?>
-
 
 
                     
@@ -118,3 +112,26 @@ if(isset($_POST['update'])) {
 
                              
 
+
+<script>
+
+    let deleteButtons = document.querySelectorAll('.btn-delete');
+  console.log("here",deleteButtons)
+    for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', function(event) {
+            var itemId = event.target.dataset.itemId;
+            console.log("datasetId", itemId);
+            var modal = document.querySelector('#deleteModal-' + itemId);
+            modal.classList.remove('hidden');
+        });
+    }
+
+    function hideModal(commentId) {
+        document.getElementById(`deleteModal-${commentId}`).classList.add("hidden");
+    }
+
+    function deletePost(commentId) {
+        // Insert code to make an HTTP request to delete the post here
+        hideModal(commentId);
+    }
+</script>

b08d099ed0736a23059e831cbd028512b6cbba35
rinori-g <rinorg11@outlook.com>
2023-02-03 14:43:59 +0100

    merging

--- wp-content/themes/starlabs/functions.php
+++ wp-content/themes/starlabs/functions.php
@@ -474,18 +474,18 @@ function search_filter($query) {
 */
 
 // Send email via SMTP
-add_action( 'phpmailer_init', 'send_smtp_email' );
-function send_smtp_email( $phpmailer ) {
-    $phpmailer->isSMTP();     
-    $phpmailer->Host = SMTP_HOST;
-    $phpmailer->SMTPAuth = SMTP_AUTH;
-    $phpmailer->Port = SMTP_PORT;
-    $phpmailer->Username = SMTP_USER;
-    $phpmailer->Password = SMTP_PASS;
-    $phpmailer->SMTPSecure = SMTP_SECURE;
-    $phpmailer->From = SMTP_FROM;
-    $phpmailer->FromName = SMTP_NAME;
-}
+// add_action( 'phpmailer_init', 'send_smtp_email' );
+// function send_smtp_email( $phpmailer ) {
+//     $phpmailer->isSMTP();     
+//     $phpmailer->Host = SMTP_HOST;
+//     $phpmailer->SMTPAuth = SMTP_AUTH;
+//     $phpmailer->Port = SMTP_PORT;
+//     $phpmailer->Username = SMTP_USER;
+//     $phpmailer->Password = SMTP_PASS;
+//     $phpmailer->SMTPSecure = SMTP_SECURE;
+//     $phpmailer->From = SMTP_FROM;
+//     $phpmailer->FromName = SMTP_NAME;
+// }
 
 function send_email_on_comment( $comment_id ) {
   $comment = get_comment( $comment_id );

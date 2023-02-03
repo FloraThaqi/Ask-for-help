52ceb57dc5fb9ab3b72096cd786a896b9963af2a
ritmiraberisha <ritmiraberisha@gmail.com>
2023-02-03 10:53:20 +0100

    correct answer

--- wp-content/themes/starlabs/partials/content-correct.php
+++ wp-content/themes/starlabs/partials/content-correct.php
@@ -1,9 +1,13 @@
 <?php
 if($author_id==$user_id){
   if ($is_correct) {
-      $text .= '<p class="absolute top-0 right-0 pr-4"><button name="is_correct" id="iscorrect">Correct</button></p>';
+      $text .= '<button name="is_correct" class="absolute top-0 right-0 mr-4" id="iscorrect">
+      <p class="text-green-600 font-bold">Correct</p>
+      </button>';
   } else {
-      $text .= '<p class="absolute top-0 right-0 pr-4"><button name="is_correct" id="iscorrect'.$comment_id.'" onclick="markAsCorrect('.$comment_id.')">Mark as correct</button></p>';
+      $text .= '<button name="is_correct" class="absolute top-0 right-0 mr-4" id="iscorrect'.$comment_id.'" onclick="markAsCorrect('.$comment_id.')" >
+      <p class="text-slate-500">Mark as correct</p>
+      </button>';
   }
 }
 ?>

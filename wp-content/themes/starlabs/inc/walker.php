bf85512a4f93c45c44613b7a6abfc4120d1bea3a
ritmiraberisha <ritmiraberisha@gmail.com>
2023-02-21 10:49:09 +0100

    border

--- wp-content/themes/starlabs/inc/walker.php
+++ wp-content/themes/starlabs/inc/walker.php
@@ -49,7 +49,7 @@ class Walker_Nav_Primary extends Walker
 		}
 		$indent = str_repeat($t, $depth);
 
-		$output .= "\n$indent<div id='double-dropdown' class=\"doubleDropdown hidden md:absolute bg-white dark:bg-[#0b1523] dark:text-white dark:border-black border-2 shadow-lg mx-auto  z-10 w-full md:w-auto md:min-w-[150px] divide-y md:py-2  \">\n";
+		$output .= "\n$indent<div id='double-dropdown' class=\"doubleDropdown hidden md:absolute bg-white dark:bg-[#0b1523] dark:text-white dark:border-[#243042] border-2 shadow-lg mx-auto  z-10 w-full md:w-auto md:min-w-[150px] divide-y md:py-2  \">\n";
 		// Default class.
 		$classes = array('sub-menu');
 
